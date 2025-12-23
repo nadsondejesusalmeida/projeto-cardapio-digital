1d4217ab9f9ff6d6ad1a3949a62087c1121b0527
nadsondejesusalmeida <developer.brownnose603@slmails.com>
2025-12-22 22:06:56 -0300

    Removido o README.md

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 				<div className="banner">
 					<img src={restaurantImage} alt="Imagem do Restaurante" />
 				</div>
-				
+
 				<Navigation
 					selectedCategory={selectedCategory}
 					setSelectedCategory={setSelectedCategory}
